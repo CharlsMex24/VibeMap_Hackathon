9c6ba008d4edc1c44ef5b8b18f5de6040da11a5a
Bruno Salas <brunich99@gmail.com>
2026-05-09 14:32:53 -0600

    Permite frontend en 127 local

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const port = 3000;
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173"];
+  : ["http://localhost:5173", "http://127.0.0.1:5173"];
 
 app.use(cors({
   origin: (origin, callback) => {
